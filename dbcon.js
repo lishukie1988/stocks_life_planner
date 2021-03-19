1fba08837ad44ddea415d43a1cf77f005edc03b1
shukieliosu <59763257+shukieliosu@users.noreply.github.com>
2021-03-19 23:46:37 +0800

    newpass

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
    connectionLimit : 10,
    host            : 'portfolio2.cbdwadacowh0.us-east-2.rds.amazonaws.com',
    user            : 'liyuk',
-   password        : 'c001!0z0mb!3osu',
+   password        : '5pammu5ub!',
    database        : 'portfolio2',
    port            : 3300
 });
